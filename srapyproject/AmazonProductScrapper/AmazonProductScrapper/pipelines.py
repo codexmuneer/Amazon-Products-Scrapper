8dc5617bde44d785f942ca73f0b11c2ff47164bf
codexmuneer <muneerhanif7@gmail.com>
2023-02-06 07:51:41 +0000

    work done

--- srapyproject/AmazonProductScrapper/AmazonProductScrapper/pipelines.py
+++ srapyproject/AmazonProductScrapper/AmazonProductScrapper/pipelines.py
@@ -5,9 +5,37 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# from itemadapter import ItemAdapter
+
+import sqlite3
+class AmazonproductscrapperPipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("myquotes.db")
+        self.curr = self.conn.cursor()
 
 
-class AmazonproductscrapperPipeline:
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS quotes_db""")
+        self.curr.execute("""CREATE TABLE quotes_tb(
+        title TEXT,
+        author TEXT,
+        tag TEXT
+        )""")
     def process_item(self, item, spider):
+        self.store_db(item)
+        print("Pipeline: " + item['title'][0] )
         return item
+
+    def store_db(self,item):
+        self.curr.execute("INSERT INTO quotes_tb VALUES (?,?,?)",[
+            (item['title'][0]),
+            (item['author'][0]),
+            (item['tag'][0])
+        ]
+        )
+        self.conn.commit()
